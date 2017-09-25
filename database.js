7f94778a89118abd57d053918a5dcf6a78a83b9b
Michael Scheurer <michael.scheurer@seccom.ch>
2017-09-25 18:08:21 +0200

    db can create database and table

--- database.js
+++ database.js
@@ -3,17 +3,28 @@
  */
 var mysql = require('mysql');
 
-    //database credentials
-    var con = mysql.createConnection( {
-        host: "localhost",
-        user: "todo-backend",
-        password: "todo-backend"
-    });
-    
-    //create database connection
-    con.connect(function(err) {
-       if(err) throw err;
-       console.log("Connected");
-    });
-    
-    
+//database credentials
+var con = mysql.createConnection( {
+    host: "localhost",
+    user: "todo-backend",
+    password: "todo-backend"
+});
+
+//create database connection
+con.connect(function(err) {
+    if(err) throw err;
+    console.log("Connected to mysql database");       
+});
+
+// Create database if not exist
+con.query("CREATE DATABASE IF NOT EXISTS todo_backend", function (err, result) {
+if (err) throw err;
+console.log("Database created");
+});
+
+//Create tables
+var sql = "CREATE TABLE IF NOT EXISTS todo_backend.todos (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), ordering INT, completed VARCHAR(255))";
+con.query(sql, function (err, result) {
+   if (err) throw err;
+   console.log("Tables created");
+});
